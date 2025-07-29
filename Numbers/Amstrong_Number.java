da055dc5f9776e9f448ac884acd5b6eb12a87997
Vidhyaa Thiyagarajan <vidhyaathiyaga@gmail.com>
2025-07-29 19:38:13 +0530

    added Amstrong Number program

---
+++ Numbers/Amstrong_Number.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Amstrong_Number
+{
+    public static void main(String[] args)
+    {
+        Scanner c=new Scanner(System.in);
+        int num=c.nextInt();
+        int count=0;
+        int temp=num;
+        while(temp!=0)
+        {
+            count++;
+            temp/=10;
+        }
+        temp=num;
+        int sum=0;
+        while(temp!=0)
+        {
+            sum+=power(temp%10,count);
+            temp/=10;
+        }
+        if(num==sum)
+        {
+            System.out.print("Amstrong Number");
+        }
+        else
+        {
+            System.out.print("Not a Amstrong Number");
+        }
+    }
+    public static int power(int base,int exp)
+    {
+        int i,res=1;
+        for(i=1;i<=exp;i+=1)
+        {
+            res *= base;
+        }
+        return res;
+    }
+}
